408146ce97d6c69d56ece56654ef51b4f6b78a72
nayemhasan45 <nayemhasan75907@gmail.com>
2022-10-09 23:24:32 -0700

    header style done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <Link to='/home'>Home</Link >
             <Link to='/orders'>Orders</Link >
             <Link to='/review'>Orders Review</Link >
